22db3c806b64e2cce7022ccb036c9844d8dbb726
mamnun123 <mahathirmehedi@gmail.com>
2026-02-19 19:29:06 +0100

    Grab is multiplayer(?)

--- Assets/Script/TrashInput.cs
+++ Assets/Script/TrashInput.cs
@@ -1,5 +1,9 @@
-using UnityEngine;
 using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 // 1. Define the Types globally so Bins can see them
 public enum TrashType { Plastic, Paper, WhiteGlass, GreenGlass, BrownGlass, Landfill, Organic}
@@ -11,6 +15,19 @@ public class TrashItem : NetworkBehaviour
 
     // We remember where we spawned so we can reset if put in wrong bin
     private Vector3 startPosition;
+
+    private NetworkObject netObj;
+    private XRGrabInteractable grabInteractable;
+
+    private void Start()
+    {
+        netObj = GetComponent<NetworkObject>();
+        grabInteractable = GetComponent<XRGrabInteractable>();
+
+        // Subscribe to grab events
+        grabInteractable.selectEntered.AddListener(OnGrab);
+        //grabInteractable.selectExited.AddListener(OnRelease);
+    }
 
     public override void OnNetworkSpawn()
     {
@@ -27,4 +44,48 @@ public class TrashItem : NetworkBehaviour
         GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
         GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
     }
+
+    void PickupObject()
+    {
+        NetworkObject.ChangeOwnership(NetworkManager.Singleton.LocalClientId);
+    }
+
+
+    // -------------------------------------------------
+
+    private void OnGrab(SelectEnterEventArgs interactor)
+    {
+        // Request ownership from server for the grabbing player
+        ulong localClientId = NetworkManager.Singleton.LocalClientId;
+        if (netObj.OwnerClientId != localClientId)
+        {
+            RequestOwnershipServerRpc(localClientId);
+        }
+    }
+
+    /*
+    private void OnRelease(SelectExitEventArgs interactor)
+    {
+        // Optional: return ownership to server when released
+        if (!IsServer)
+        {
+            ReturnOwnershipToServerServerRpc();
+        }
+    }
+    */
+
+    [ServerRpc]
+    private void RequestOwnershipServerRpc(ulong clientId)
+    {
+        netObj.ChangeOwnership(clientId);
+    }
+
+    /*
+    [ServerRpc]
+    private void ReturnOwnershipToServerServerRpc()
+    {
+        netObj.ChangeOwnership(NetworkManager.ServerClientId);
+    }
+    */
+
 }
